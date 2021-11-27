b483362b998940eda9a0b7e4cfb03c53a052cb14
krcs <k.netpost@gmail.com>
2021-11-27 20:20:39 +0100

    Powershell - clock.ps1

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Terminal clock
 
-### Python curses.
+### Python with curses.
 
 ```
 usage: clock.py [-margin MARGIN] font
@@ -9,4 +9,15 @@ usage: clock.py [-margin MARGIN] font
 Example:
 ```
 ./clock.py -margin=3 ./font/f12x9.txt
+```
+
+### Powershell
+
+```
+usage: clock.ps1 [-margin MARGIN] font
+```
+
+Example:
+```
+./clock.ps1 -margin 2 ./font/f6x5.txt
 ```
---
+++ clock.ps1
@@ -0,0 +1,117 @@
+#!/usr/bin/pwsh
+
+param(
+    [Parameter(mandatory=$true)]
+    [string]$font,
+    [Parameter()]
+    [int]$margin = 0
+)
+
+if (-not (Test-Path $font)) {
+    write-host -Foreground RED "Font not found."
+    exit
+}
+
+if ($margin -lt 0) {
+    $margin = 0
+}
+
+function get_size($content) {
+    $font_width = 0
+    $font_height = 0
+
+    $height = 0
+    $margin = 0
+
+    foreach ($line in $content) {
+        if ($line.Length -eq 0) {
+            if ($height -gt 0) {
+                $font_height = $height
+            }
+            $height = 0
+            continue
+        }
+
+        if ($line.Length -gt $font_width) {
+            $font_width = $line.Length
+        }
+
+        $height += 1
+
+        $line | ? { $_ -match "^\s*" } | % {
+            $slen = $matches[0].Length
+            if ($margin -eq 0) {
+                $margin = $slen
+            } elseif ($slen -lt $margin) {
+                $margin = $slen
+            }
+        }
+    }
+    return @{
+        width = ($font_width - $margin)
+        height = $font_height
+    }
+}
+
+function get_chars($content, $font_size) {
+    $result = @()
+    $char = @()
+
+    foreach ($line in $content) {
+        if ($line.Length -eq 0) {
+            if ($char.Length -gt 0) {
+                $result += ,$char
+            }
+            $char = @()
+            continue
+        }
+        $char += $line.Substring($line.Length - $font_size.width)
+    }
+    if ($char.Length -gt 0) {
+        $result += ,$char
+    }
+    return $result
+}
+
+$content = get-content $font
+$font_size = get_size $content
+$chars = get_chars $content $font_size
+
+$win_width = (7 * ($font_size.width + $margin)) + $font_size.width
+$win_height = $font_size.height
+
+$wx = [int](([console]::LargestWindowWidth-$win_width) / 2)
+$wy = [int](([console]::LargestWindowHeight-$win_height) / 2)
+
+$posx, $posy = $wx, $wy
+$xoffset = $font_size.width + $margin
+
+[console]::CursorVisible = $false
+
+while ($true) {
+    if ([console]::KeyAvailable) {
+        [console]::ReadKey("NoEcho,IncludeKeyDown") | Out-Null
+        break
+    }
+    if ([console]::LargestWindowWidth -lt $wx + $win_width) {
+        Clear-Host
+        break
+    }
+    $posx = $wx
+    (Get-Date -UFormat "%H:%M:%S")[0..9] | % {
+        $idx = $chars.Length - 1
+        if ($_ -ne ":") {
+            $idx = [int]"$_"
+        }
+        0..($font_size.height - 1) | % {
+            [console]::SetCursorPosition($posx, $wy + $_)
+            $chars[$idx][$_]
+        }
+        $posx += $xoffset
+    }
+    [console]::SetCursorPosition(0, $posy + 1)
+    Start-Sleep -Seconds 1
+}
+[console]::CursorVisible = $true
+
+Clear-Host
